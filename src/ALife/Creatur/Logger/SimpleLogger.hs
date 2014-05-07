a71886eef0de02805022fe6c253c18da4988a59f
Amy de Buitléir <amy@nualeargais.ie>
2014-05-07 11:05:47 +0100

    v5.4.1

---
+++ src/ALife/Creatur/Logger/SimpleLogger.hs
@@ -0,0 +1,59 @@
+------------------------------------------------------------------------
+-- |
+-- Module      :  ALife.Creatur.Logger.SimpleLogger
+-- Copyright   :  (c) Amy de Buitléir 2011-2014
+-- License     :  BSD-style
+-- Maintainer  :  amy@nualeargais.ie
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- A simple rotating log, tailored to the needs of the Créatúr 
+-- framework.
+--
+------------------------------------------------------------------------
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+
+module ALife.Creatur.Logger.SimpleLogger
+  (
+    SimpleLogger,
+    mkSimpleLogger
+  ) where
+
+import ALife.Creatur.Util (getLift)
+import ALife.Creatur.Logger (Logger(..), timestamp)
+import Control.Conditional (unlessM)
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.State (StateT, gets, modify)
+import System.Directory (createDirectoryIfMissing)
+import System.FilePath (splitFileName)
+
+-- | A rotating logger.
+data SimpleLogger = SimpleLogger {
+    initialised :: Bool,
+    logFilename :: FilePath
+  } deriving (Show, Eq)
+
+-- | @'mkSimpleLogger' f@ creates a logger that will write to
+--   file @f@.
+mkSimpleLogger :: FilePath -> SimpleLogger
+mkSimpleLogger f = SimpleLogger False f
+
+instance Logger SimpleLogger where
+  writeToLog msg = do
+    initIfNeeded
+    getLift $ write' msg
+
+initIfNeeded :: StateT SimpleLogger IO ()
+initIfNeeded =
+  unlessM (gets initialised) initialise
+
+initialise :: StateT SimpleLogger IO ()
+initialise = do
+  (d,_) <- fmap splitFileName $ gets logFilename
+  liftIO $ createDirectoryIfMissing True d
+  modify (\l -> l { initialised=True } )
+
+write' :: String -> SimpleLogger -> IO ()
+write' msg logger = do
+  ts <- timestamp
+  appendFile (logFilename logger) $ ts ++ "\t" ++ msg ++ "\n"
---
+++ src/ALife/Creatur/Logger/SimpleRotatingLogger.hs
@@ -0,0 +1,125 @@
+------------------------------------------------------------------------
+-- |
+-- Module      :  ALife.Creatur.Logger.SimpleRotatingLogger
+-- Copyright   :  (c) Amy de Buitléir 2011-2014
+-- License     :  BSD-style
+-- Maintainer  :  amy@nualeargais.ie
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- A simple rotating log, tailored to the needs of the Créatúr 
+-- framework.
+--
+------------------------------------------------------------------------
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+
+module ALife.Creatur.Logger.SimpleRotatingLogger
+  (
+    Logger(..),
+    SimpleRotatingLogger,
+    mkSimpleRotatingLogger
+  ) where
+
+import ALife.Creatur.Util (getLift)
+import ALife.Creatur.Logger (Logger(..), timestamp)
+import Control.Conditional (whenM, unlessM)
+import Control.Monad (when)
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.State (StateT, gets, modify)
+import System.Directory (createDirectoryIfMissing, doesFileExist,
+  renameFile)
+
+-- | A rotating logger.
+data SimpleRotatingLogger = SimpleRotatingLogger {
+    initialised :: Bool,
+    directory :: FilePath,
+    logFilename :: FilePath,
+    expFilename :: FilePath,
+    maxRecordsPerFile :: Int,
+    recordCount :: Int,
+    logCount :: Int
+  } deriving (Show, Eq)
+
+-- | @'mkSimpleRotatingLogger' d prefix n@ creates a logger that will write to
+--   directory @d@. The log \"rotates\" (starts a new log file) every @n@
+--   records. Log files follow the naming convention @prefix@./k/, where /k/ 
+--   is the number of the last log record contained in the file. If logging
+--   has already been set up in @directory@, then logging will continue where
+--   it left off; appending to the most recent log file.
+mkSimpleRotatingLogger :: FilePath -> String -> Int -> SimpleRotatingLogger
+mkSimpleRotatingLogger d pre n =
+  SimpleRotatingLogger False d fLog fExp n 0 0
+  where fLog = d ++ "/" ++ pre ++ ".log"
+        fExp = d ++ "/" ++ pre ++ ".exp"
+
+instance Logger SimpleRotatingLogger where
+  writeToLog msg = do
+    initIfNeeded
+    bumpRecordCount
+    rotateLogIfNeeded
+    getLift $ write' msg
+    saveState
+
+initIfNeeded :: StateT SimpleRotatingLogger IO ()
+initIfNeeded =
+  unlessM (gets initialised) initialise
+
+initialise :: StateT SimpleRotatingLogger IO ()
+initialise = do
+  d <- gets directory
+  liftIO $ createDirectoryIfMissing True d
+  fExp <- gets expFilename
+  whenM (liftIO $ doesFileExist fExp) readState
+  modify (\l -> l { initialised=True } )
+  debug "initialise"
+
+debug :: String -> StateT SimpleRotatingLogger IO ()
+debug s = do
+  n <- gets recordCount
+  k <- gets logCount
+  getLift . write' $ "DEBUG " ++ s ++ ": n=" ++ show n ++ ": k=" ++ show k 
+  fExp <- gets expFilename
+  whenM (liftIO $ doesFileExist fExp) $ do
+    s' <- liftIO $ readFile fExp
+    let (n',k') = read s' :: (Int,Int)
+    getLift . write' $ "DEBUG2 " ++ s ++ ": n'=" ++ show n' ++ ": k'=" ++ show k' 
+  
+readState :: StateT SimpleRotatingLogger IO ()
+readState = do
+  fExp <- gets expFilename
+  s <- liftIO $ readFile fExp
+  let (n,k) = read s
+  modify (\l -> l { recordCount=n, logCount=k } )
+
+saveState :: StateT SimpleRotatingLogger IO ()
+saveState = do
+  e <- gets expFilename
+  n <- gets recordCount
+  k <- gets logCount
+  liftIO . writeFile e $ "(" ++ show n ++ "," ++ show k ++ ")"
+
+write' :: String -> SimpleRotatingLogger -> IO ()
+write' msg logger = do
+  ts <- timestamp
+  appendFile (logFilename logger) $ ts ++ "\t" ++ msg ++ "\n"
+
+bumpRecordCount :: StateT SimpleRotatingLogger IO ()
+bumpRecordCount = modify (\l -> l { recordCount=recordCount l + 1 })
+
+rotateLogIfNeeded :: StateT SimpleRotatingLogger IO ()
+rotateLogIfNeeded = do
+  debug "rotateLogIfNeeded"
+  n <- gets recordCount
+  m <- gets maxRecordsPerFile
+  when (n >= m) $ rotateLog
+  
+rotateLog :: StateT SimpleRotatingLogger IO ()
+rotateLog = do
+  debug "rotateLog"
+  f <- gets logFilename
+  n <- gets logCount
+  let fPrev = f ++ '.' : show n
+  getLift . write' $ "Continued in log " ++ show (n+1)
+  liftIO $ renameFile f fPrev
+  getLift . write' $ "Continued from log " ++ show n
+  modify (\l -> l { recordCount=0, logCount=n+1 })
