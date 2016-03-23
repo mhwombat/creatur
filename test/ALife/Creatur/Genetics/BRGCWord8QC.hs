c5033f719020e721ce97638832c5e29e9d7a9ca7
Amy de Buitléir <amy@nualeargais.ie>
2016-03-23 16:27:20 +0000

    added some tests

--- test/ALife/Creatur/Genetics/BRGCWord8QC.hs
+++ test/ALife/Creatur/Genetics/BRGCWord8QC.hs
@@ -26,8 +26,8 @@ import Data.Word (Word8, Word16, Word32, Word64)
 import GHC.Generics (Generic)
 import Test.Framework as TF (Test, testGroup)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
-import Test.QuickCheck (Arbitrary, Gen, Property, arbitrary, choose,
-  oneof, property, sized, vectorOf)
+import Test.QuickCheck ((==>), Arbitrary, Gen, Property, arbitrary,
+  choose, oneof, property, sized, vectorOf)
 
 #if MIN_VERSION_base(4,8,0)
 #else
@@ -38,6 +38,13 @@ prop_round_trippable :: (Eq g, Genetic g) => g -> Property
 prop_round_trippable g = property $ g' == Right g
   where x = write g
         g' = read x
+
+prop_round_trippable2
+  :: Genetic g => Int -> [Word8] -> g -> Property
+prop_round_trippable2 n xs dummy = length xs >= n
+  ==> xs' == take n xs
+  where Right g = read xs
+        xs' = write (g `asTypeOf` dummy)
 
 prop_rawWord8s_round_trippable :: [Word8] -> Property
 prop_rawWord8s_round_trippable g = property $ g' == g
@@ -84,6 +91,14 @@ test = testGroup "ALife.Creatur.Genetics.BRGCWord8QC"
       (prop_round_trippable :: Word32 -> Property),
     testProperty "prop_round_trippable - Word64"
       (prop_round_trippable :: Word64 -> Property),
+    testProperty "prop_round_trippable2 - Word8"
+      (prop_round_trippable2 1 :: [Word8] -> Word8 -> Property),
+    testProperty "prop_round_trippable2 - Word16"
+      (prop_round_trippable2 2 :: [Word8] -> Word16 -> Property),
+    testProperty "prop_round_trippable2 - Word32"
+      (prop_round_trippable2 4 :: [Word8] -> Word32 -> Property),
+    testProperty "prop_round_trippable2 - Word64"
+      (prop_round_trippable2 8 :: [Word8] -> Word64 -> Property),
     testProperty "prop_round_trippable - TestStructure"
       (prop_round_trippable :: TestStructure -> Property),
     testProperty "prop_rawWord8s_round_trippable"
